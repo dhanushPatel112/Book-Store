25cee46f97deef554f4dbf3907809eb3186a0bda
spockpatel <idforfack@gmail.com>
2021-04-21 01:40:58 +0530

    try 7

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,7 +53,7 @@ exports.signin = (req, res) => {
             });
         }
         // generate a signed token with user id and secret
-        const token = jwt.sign({ _id: user._id }, "adsfkasfdheqwoifncasndmlkasxd");
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
         // persist the token as 't' in cookie with expiry date
         res.cookie('t', token, { expire: new Date() + 9999 });
         // return response with user and token to frontend client
@@ -68,7 +68,7 @@ exports.signout = (req, res) => {
 };
 
 exports.requireSignin = expressJwt({
-    secret: "adsfkasfdheqwoifncasndmlkasxd",
+    secret: process.env.JWT_SECRET,
     userProperty: 'auth'
 });
 
--- controllers/braintree.js
+++ controllers/braintree.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 
 const gateway = braintree.connect({
     environment: braintree.Environment.Sandbox,
-    merchantId: "gngpzft8ch72f7tm",
-    publicKey: "xdxts4sw7h99jbbp",
-    privateKey: "66b88edcbbf9902620e635c015613b55"
+    merchantId: process.env.BRAINTREE_MERCHANT_ID,
+    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+    privateKey: process.env.BRAINTREE_PRIVATE_KEY
 });
 
 exports.generateToken = (req, res) => {
